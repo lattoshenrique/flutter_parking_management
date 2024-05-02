e46b9e9baa25a9a9ac09735426b5817c1e0e709a
Henrique <henrique.brito.3@globalhitss.com.br>
2024-05-02 16:05:44 -0300

    create vehicle package

---
+++ packages/vehicles/lib/src/domain/entities/vehicle.dart
@@ -0,0 +1,12 @@
+import 'package:common_deps/common_deps.dart';
+
+part 'vehicle.freezed.dart';
+
+@freezedNoHelpers
+sealed class Vehicle with _$Vehicle {
+  const factory Vehicle({
+    required String id,
+    required String name,
+    @Default('') String plate,
+  }) = _Vehicle;
+}
